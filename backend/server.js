5de74b4dba7fe1d4a73c240e1502b9322daf4fa7
teddyabe6 <teddyabe6@gmail.com>
2025-05-26 19:13:58 +0300

    MERN Practise starter

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+
+const app =  express();
+
+app.get("/", (req, res) => {
+    res.send("server is ready")
+})
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("server started at ")
+})
+
