ec7b5f0f0723c6b72ea841a6cb0878b5b0ee5559
teddyabe6 <teddyabe6@gmail.com>
2025-05-29 13:38:17 +0300

    completed backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js"
 
 dotenv.config();
 
 const app =  express();
 
-app.get("/", (req, res) => {
-    res.send("server is ready")
-})
+app.use(express.json());
+
+app.use("/api/products",productRoutes)
 
 app.listen(5000, () => {
     connectDB();
